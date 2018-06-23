347b35410931d206b46661a0bdb8b78289b436c5
Veaceslav Medvedev <slavcopost@gmail.com>
2018-06-23 03:06:45 +0200

    Init PhpUnit project

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of {@see https://github.com/slavcodev/ Slavcodev Projects}.
+ */
+
+declare(strict_types=1);
+
+namespace Symfony\Component\Routing\Tests\Loader;
+
+use PHPUnit\Framework;
+
+abstract class TestCase extends Framework\TestCase
+{
+}
